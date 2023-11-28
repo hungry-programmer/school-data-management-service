ff0c136208a2bfe64532d66ad65fe91419d4e31b
gulu <davidgulua9@gmil.com>
2023-11-28 21:26:03 +0400

    Initial commit

---
+++ src/main/java/com/schooldms/schooldataservice/SchoolDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.schooldms.schooldataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolDataServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schooldms/schooldataservice/SchoolDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schooldms.schooldataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
